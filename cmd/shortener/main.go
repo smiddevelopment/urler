d7be80b6d6c82254557c52575f0281eb3c197726
smiddevelopment <smiddevelopment@gmail.com>
2023-07-12 12:36:28 +0300

    test1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/smiddevelopment/urler.git/internal/app/handler"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, handler.EncodeUrl)
+	mux.HandleFunc(`/{id}`, handler.DecodeUrl)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
