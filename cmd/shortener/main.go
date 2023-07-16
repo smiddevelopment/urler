4fc1c7bb04336bf45c736ed30142be8d6c362d14
smiddevelopment <smiddevelopment@gmail.com>
2023-07-16 19:40:53 +0300

    add new inter3

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,30 +2,48 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/smiddevelopment/urler.git/internal/app/handler"
 )
 
-var port, resUrl *string
+var options struct {
+	port   string
+	resUrl string
+}
 
 func init() {
-	port = flag.String("a", "8080", "-a is the server path")
-	resUrl = flag.String("b", "http://localhost:8080", "-b <base address result>")
-	flag.Parse()
+	flag.StringVar(&options.port, "a", "8080", "-a is the server path")
+	flag.StringVar(&options.resUrl, "b", "http://localhost:8080", "-b <base address result>")
 }
 
 func main() {
+	flag.Parse()
+
+	servAddr, exist := os.LookupEnv("USERNAME")
+	if exist {
+		// Print the value of the environment variable
+		fmt.Print(servAddr)
+	}
+
+	baseUrl, exist := os.LookupEnv("BASE_URL")
+	if exist {
+		// Print the value of the environment variable
+		fmt.Print(baseUrl)
+	}
+
 	e := echo.New()
 
-	decodeHandler := handler.New(resUrl)
+	decodeHandler := handler.New(&options.resUrl)
 	e.POST(`/`, decodeHandler.EncodeUrl)
 	e.GET(`/{id}`, decodeHandler.DecodeUrl)
 
 	defaultPort := "5000"
 
-	if port != nil {
-		defaultPort = *port
+	if &options.port != nil {
+		defaultPort = options.port
 	}
 
 	e.Logger.Fatal(e.Start(":" + defaultPort))
