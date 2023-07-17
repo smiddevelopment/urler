69f915dc9937a7f8e04050a589fecc46d39177b0
smiddevelopment <smiddevelopment@gmail.com>
2023-07-18 00:24:30 +0300

    test 4 iter 4

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -11,7 +11,7 @@ import (
 var port *string
 
 func init() {
-	port = flag.String("a", "localhost:8888", "-a server port")
+	port = flag.String("a", "localhost:8080", "-a server port")
 	flag.String("b", "http://localhost:8080/", "-b result URL address")
 }
 
