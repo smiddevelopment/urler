edabf7696d3b1a0128301b705f6076ab38e4479a
smiddevelopment <smiddevelopment@gmail.com>
2023-07-17 21:36:47 +0300

    test 11 iter 1

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -20,19 +20,19 @@ func Add(url string) string {
 		ID:  generateRandomID(),
 		URL: url,
 	}
-
+	// Проверка массива на пустоту и инициализация
 	if EncodedURLs == nil {
 		EncodedURLs = []EncodedURL{newURL}
 
 		return newURL.ID
 	}
-
+	// Поиск существующей ссылки по URL в массиве
 	for i := 0; i < len(EncodedURLs); i++ {
 		if url == EncodedURLs[i].URL {
 			return EncodedURLs[i].ID
 		}
 	}
-
+	// Добавление новой ссылки, если она не была найдена ранее
 	_ = append(EncodedURLs, newURL)
 
 	return newURL.ID
@@ -40,6 +40,7 @@ func Add(url string) string {
 
 // Get поиск существующей в хранилище ссылки по ID
 func Get(id string) string {
+	// Поиск существующей ссылки по ID в массиве
 	for i := 0; i < len(EncodedURLs); i++ {
 		if id == EncodedURLs[i].ID {
 			return EncodedURLs[i].URL
